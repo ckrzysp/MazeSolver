21db88827482d2c1dd3e342308262882697c69ea
lausan3 <lauanthony4455@gmail.com>
2025-05-11 17:02:39 -0400

    Add Anthony and Thomas CPU 10 epoch results

--- model/gpu_training.py
+++ model/gpu_training.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 def train_model():
     model = YOLO("yolov8n.pt")
-    model.train(data="data.yaml", epochs=10, imgsz=640, device=0)
+    model.train(data="data.yaml", epochs=10, imgsz=640, device="cpu")
 
 if __name__ == "__main__":  # Protect the main entry point
     train_model()
